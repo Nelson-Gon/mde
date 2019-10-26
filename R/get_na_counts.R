d4b5bdad960a8dc06a2ee8138447ee8d0514a10b
Nelson-Gon <gonzabato@hotmail.com>
2019-10-27 00:10:29 +0800

    Support grouped counts

--- R/get_na_counts.R
+++ R/get_na_counts.R
@@ -1,18 +1,38 @@
-#' Add columnwise counts of missing values
+#' Add columnwise/groupwise counts of missing values
 #' @param x A valid R `object` for which `na_counts` are needed.
+#' @param grouped Logical. Do you need to add grouped counts? Defaults to `FALSE`
+#' @param grouping_cols If `grouped` is set to `TRUE`, one can provide the columns by which to group the
+#' data.
 #' @description This function takes a `data.frame` object as an input and returns the corresponding
 #' `NA` counts. `NA` refers to R's builtin missing data holder.
-#' @return An object of the same type as `x` showing the respective number of missing values.
+#' @importFrom magrittr %>%
+#' @return An object of the same type as `x` showing the respective number of missing values. If
+#' grouped is set to `TRUE`, the results are returned by group.
 #' @examples
 #' get_na_counts(airquality)
+#' # Grouped counts
+#' test <- data.frame(Subject = c("A","A","B","B"), res = c(NA,1,2,3),
+#' ID = c("1","1","2","2"))
+#' get_na_counts(test,grouped = FALSE, grouping_cols =
+#'                         c("ID", "Subject"))
 #' @export
 
-get_na_counts <- function(x){
+get_na_counts <- function(x, grouped = FALSE,
+                          grouping_cols = NULL){
   UseMethod("get_na_counts")
 }
 
 #' @export
 
-get_na_counts.data.frame <- function(x){
-  lengths(sapply(x,function(column) which(is.na(column))))
+get_na_counts.data.frame <- function(x, grouped = FALSE,
+                                     grouping_cols = NULL){
+  if(grouped){
+    x %>%
+    dplyr::group_by(!!!dplyr::syms(grouping_cols)) %>%
+      dplyr::summarise_all(list(function(column) sum(is.na(column))))
+  }
+  else{
+    lengths(sapply(x,function(column) which(is.na(column))))
+  }
+
 }
