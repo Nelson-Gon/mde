c1f49f171da5e7120f5617d0caae6a3dfb54ea5b
Nelson-Gon <gonzabato@hotmail.com>
2019-10-27 12:02:33 +0800

    Support grouped percentages

--- R/percent_missing.R
+++ R/percent_missing.R
@@ -3,6 +3,7 @@
 #' @param percent Logical. Should values be displayed as percentages? Defaults to `TRUE`, otherwise
 #' results are shown in decimals.
 #' @description A convenient way to obtain percent missingness columnwise.
+#' @inheritParams get_na_counts
 #' @return An object of the same class as x showing the percentage of missing values.
 #' @examples
 #' percent_missing(airquality)
@@ -10,17 +11,43 @@
 #' percent_missing(airquality, percent = FALSE)
 #' @export
 
-percent_missing <- function(x, percent = TRUE){
+percent_missing <- function(x, percent = TRUE, grouped = FALSE,
+                            grouping_cols = NULL){
   UseMethod("percent_missing")
 }
 
 #' @export
-percent_missing.data.frame <- function(x, percent = TRUE){
+percent_missing.data.frame <- function(x, percent = TRUE,
+                                       grouped = FALSE,
+                                       grouping_cols = NULL
+                                       ){
+
+# Check that user actually provides grouping columns
+  if(grouped & is.null(grouping_cols)){
+    warning("You chose to do grouping but no grouping columns were provided.")
+  }
 
   if(percent){
-    purrr::map_dbl(x, ~sum(is.na(.x))/ length(.x) * 100)
+    if(grouped){
+      # Probably better exported to avoid repetition
+      # Can leave as is for now
+      x %>%
+        dplyr::group_by(!!!dplyr::syms(grouping_cols)) %>%
+        dplyr::summarise_all(list(function(x)
+          sum(is.na(x))/length(x) * 100))
+    }
+    else{
+      purrr::map_dbl(x, ~sum(is.na(.x))/ length(.x) * 100)
+
+    }
+
   }
   else{
+    if(grouped){
+      x %>%
+        dplyr::group_by(!!!dplyr::syms(grouping_cols)) %>%
+        dplyr::summarise_all(list(function(x) sum(is.na(x))/length(x)))
+    }
     purrr::map_dbl(x, ~sum(is.na(.x))/ length(.x))
   }
 }
