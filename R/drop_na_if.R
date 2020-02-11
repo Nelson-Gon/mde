c1ed33e924b79b06d15c1066c24f846c7fd142ea
Nelson-Gon <gonzabato@hotmail.com>
2020-02-11 15:32:10 +0800

    Fixes #9(for now)

--- R/drop_na_if.R
+++ R/drop_na_if.R
@@ -1,9 +1,10 @@
 #' Conditon based dropping of columns with missing values
 #' @description "drop_na_if" provides a simple way to drop columns with missing values if
 #' they meet certain criteria/conditions.
-#' @param x A data.frame object
+#' @param df A data.frame object
 #' @param percent_na The percentage to use when dropping columns with missing values
 #' @param ... Other arguments to "percent_missing"
+#' @param keep_columns Columns that should be kept despite meeting the target percent_na criterion(criteria)
 #' @param sign Character. One of gteq,lteq,lt,gt or eq which refer to greater than(gt) or equal(eq) or
 #' less than(lt) or equal to(eq) respectively.
 #' @return A data.frame object with columns  that meet the target criteria dropped
@@ -12,16 +13,20 @@
 #' drop_na_if(airquality, percent_na = 24)
 #' #drop columns that have less tan or equal to 4%
 #' drop_na_if(airquality,sign="lteq", percent_na = 4)
+#' # Drop all except Ozone
+#' drop_na_if(airquality, percent_na = 24, keep_columns = "Ozone")
 #' @export
 
-drop_na_if <- function(x, sign="gteq",percent_na= 50,
+drop_na_if <- function(df, sign="gteq",percent_na= 50,
+                       keep_columns = NULL,
                        ...){
   UseMethod("drop_na_if")
 }
 
 #' @export
 
-drop_na_if.data.frame <- function(x,sign="gteq",percent_na = 50,
+drop_na_if.data.frame <- function(df,sign="gteq",percent_na = 50,
+                                  keep_columns  = NULL,
                                   ...){
   # Get column missingness
   # First use >= percent_na
@@ -31,19 +36,23 @@ drop_na_if.data.frame <- function(x,sign="gteq",percent_na = 50,
   # Instead, we shall use a switch statement which some consider
   # syntactic sugar
 
+# Currently does double work ie gets percents then returns those columns
+# Need a way to actually do it in one function
+
 to_keep<- switch(sign,
        gteq = names(Filter(function(col_percentage) ! col_percentage >= percent_na ,
-                             mde::percent_missing(x,...))),
+                             mde::percent_missing(df,...))),
          lteq = names(Filter(function(col_percentage) ! col_percentage <= percent_na,
-                           mde::percent_missing(x,...))),
+                           mde::percent_missing(df,...))),
            gt = names(Filter(function(col_percentage) !col_percentage > percent_na,
-                                 mde::percent_missing(x,...))),
+                                 mde::percent_missing(df,...))),
               lt = names(Filter(function(col_percentage) ! col_percentage < percent_na,
-                                 mde::percent_missing(x,...))),
+                                 mde::percent_missing(df,...))),
                 eq = names(Filter(function(col_percentage) !
                                      col_percentage == percent_na,
-                                mde::percent_missing(x,...))))
-x[to_keep]
+                                mde::percent_missing(df,...))))
+to_keep <- c(to_keep,keep_columns)
+df[to_keep]
 }
 
 
