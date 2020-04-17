405b4941dcb70cc5f9f9a973aa83f649ab735c3e
Nelson-Gon <gonzabato@hotmail.com>
2020-04-17 15:40:49 +0800

    Refactors drop_na_if

--- R/drop_na_if.R
+++ R/drop_na_if.R
@@ -13,8 +13,8 @@
 #' head(drop_na_if(airquality, percent_na = 24))
 #' #drop columns that have less tan or equal to 4%
 #' head(drop_na_if(airquality,sign="lteq", percent_na = 4))
-#' # Drop all except Ozone
-#' head(drop_na_if(airquality, percent_na = 24, keep_columns = "Ozone"))
+#' # Drop all except with greater than oe equal to 4% missing but keep Ozone
+#' head(drop_na_if(airquality, sign="gteq",percent_na = 4, keep_columns = "Ozone"))
 #' @export
 
 drop_na_if <- function(df, sign="gteq",percent_na= 50,
@@ -28,17 +28,8 @@ drop_na_if <- function(df, sign="gteq",percent_na= 50,
 drop_na_if.data.frame <- function(df,sign="gteq",percent_na = 50,
                                   keep_columns  = NULL,
                                   ...){
-  # Get column missingness
-  # First use >= percent_na
-  # Can change this later
-  # Solution to the above
-  # We could use eval and parse but that is frowned upon
-  # Instead, we shall use a switch statement which some consider
-  # syntactic sugar
 
-# Currently does double work ie gets percents then returns those columns
-# Need a way to actually do it in one function
-  available_options <- c("gteq","lteq","gt","lt","eq")
+available_options <- c("gteq","lteq","gt","lt","eq")
 
 if(! sign %in% available_options ) {
   stop(paste(paste(c("I was expecting one of ",available_options),collapse=" "),
@@ -46,20 +37,29 @@ if(! sign %in% available_options ) {
               sign))
 }
 
-to_keep<- switch(sign,
-       gteq = names(Filter(function(col_percentage) ! col_percentage >= percent_na ,
-                             mde::percent_missing(df,...))),
-         lteq = names(Filter(function(col_percentage) ! col_percentage <= percent_na,
-                           mde::percent_missing(df,...))),
-           gt = names(Filter(function(col_percentage) !col_percentage > percent_na,
-                                 mde::percent_missing(df,...))),
-              lt = names(Filter(function(col_percentage) ! col_percentage < percent_na,
-                                 mde::percent_missing(df,...))),
-                eq = names(Filter(function(col_percentage) !
-                                     col_percentage == percent_na,
-                                mde::percent_missing(df,...))))
-to_keep <- c(to_keep,keep_columns)
-df[to_keep]
+# get percent missingness
+
+missing_percents <-  percent_missing(df,...)
+
+to_drop<- switch(sign,
+       gteq = which(missing_percents >=percent_na),
+         lteq =  which(missing_percents <=percent_na),
+           gt =  which(missing_percents >percent_na),
+              lt = which(missing_percents <percent_na),
+                eq = which(missing_percents == percent_na))
+
+if(!is.null(keep_columns)){
+  if(!all(keep_columns %in% names(df))){
+    stop("All columns to keep should exist in the data set.")
+  }
+  keep_columns <- which(names(df) %in% keep_columns)
+
+}
+to_drop <- setdiff(to_drop, keep_columns)
+if(length(to_drop) ==0){
+  return(df)
+}
+df[-to_drop]
 }
 
 
--- tests/testthat/test_drop_na_if.R
+++ tests/testthat/test_drop_na_if.R
@@ -1,9 +1,20 @@
 testthat::test_that(desc="drop_na_if errors as expected",
-                    code = 
+                    code =
                       {
-                        
+
                 testthat::expect_error(drop_na_if(airquality,sign="nope"),
                         "I was expecting one of  gteq lteq gt lt eq not nope",
                         fixed=TRUE)
-    
+
+          testthat::expect_true("Ozone" %in% names(drop_na_if(airquality,
+                                     sign="gteq", percent_na=4,
+                                      keep_columns = "Ozone")))
+          # cannot drop both Solar.R and Ozone
+          keep_cols_test<-drop_na_if(airquality,
+                     sign="gteq", percent_na=4,keep_columns =
+                       c("Ozone","Solar.R"))
+          testthat::expect_true(ncol(keep_cols_test) == ncol(airquality))
+          testthat::expect_error(drop_na_if(airquality,keep_columns = "gibberish"),
+                                 "All columns to keep should exist in the data set.",
+                                 fixed=TRUE)
                       })
