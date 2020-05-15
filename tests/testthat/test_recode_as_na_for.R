b9a14583f5d709d218e936b867977e53b54a0906
Nelson-Gon <gonzabato@hotmail.com>
2020-05-15 21:27:30 +0800

    Improve cov rasnafor #14

--- tests/testthat/test_recode_as_na_for.R
+++ tests/testthat/test_recode_as_na_for.R
@@ -19,17 +19,30 @@ testthat::test_that(desc = "test recode_as_na_for",
                                                         value = 25,
                                                       subset_cols = "Ozone")[[2]][1]))
           # expect false since first value is not less than 25
-          testthat::expect_false(is.na(recode_as_na_for(airquality, criteria = "lt",
+          expect_false(is.na(recode_as_na_for(airquality, criteria = "lt",
                                             value = 25,
                                   subset_cols = "Ozone")[[2]][1]))
-          # should be true since we're not subset
-          testthat::expect_true(all(is.na(recode_as_na_for(airquality,
+          # expect true
+          expect_true(is.na(recode_as_na_for(airquality,criteria = "eq",subset_cols = "Solar.R",
+                                            value=190)[[2]][1]))
+         expect_true(is.na(recode_as_na_for(airquality,criteria = "lteq",subset_cols = "Solar.R",
+                                             value=190)[[2]][1]))
+         testthat::expect_false(is.na(recode_as_na_for(airquality, criteria = "gt",
+                                                       value = 25,
+                                                       subset_cols = "Ozone")[[2]][1]))
+
+          # should be true since we're not subsetting
+          expect_true(all(is.na(recode_as_na_for(airquality,
                                                            criteria = "gt",
                                        value=0)[1:2,c(1,2)])))
-         testthat::expect_true(is.na(recode_as_na_for(airquality,
+        expect_true(is.na(recode_as_na_for(airquality,
                                                       criteria="eq",
                                                       value = 41)[1,1]))
+        head(airquality)
 
+        expect_true(all(is.na(recode_as_na_for(airquality,criteria = "gteq",value = 118)[[2]][1:2])))
+        expect_false(is.na(recode_as_na_for(airquality,criteria = "lteq",value=12)[[1]][1]))
+        expect_false(is.na(recode_as_na_for(airquality,criteria = "lt",value=12)[[1]][1]))
 
 
 
