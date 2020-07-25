866c7bdf5b9dba1e5ddb00decd1bad92c8883f55
Nelson-Gon <gonzabato@hotmail.com>
2020-07-26 00:57:50 +0800

    Fix exports

--- R/column_based_recode.R
+++ R/column_based_recode.R
@@ -2,20 +2,21 @@
 #' @param  values_from Character. Name of column to get the original values from
 #' @param criterion Currently supports one of all_na or any_na to index rows that are either all NA or contain any NA.
 #' @param values_to  Character New column name for the newly recoded values. Defaults to the same name if none is supplied.
- #' @inheritParams recode_as_na 
+#' @inheritParams recode_as_na 
 #' @title Conditionally Recode NA values based on other Columns 
 #' @return A `data.frame` object with target `NA` values replaced.
 #' @examples 
-#' df <- structure(list(id = 40:43, v1 = c(NA, 1L, 1L, 1L), v2 = c(NA, 1L, 1L, 1L), v3 = c(NA, 2L, NA, 1L), 
+#' df <- structure(list(id = 40:43, v1 = c(NA, 1L, 1L, 1L), v2 = c(NA, 1L, 1L, 1L), 
+#' v3 = c(NA, 2L, NA, 1L), 
 #' test = c(1L, 2L, 1L, 3L)), class = "data.frame", row.names = c(NA, -4L))
 #' # recode test as 0 if all NA, return test otherwise
-#'  column_based_recode(df,values_from = "test", pattern_type = "starts_with", pattern="v")
-#'  @export
+#' (df,values_from = "test", pattern_type = "starts_with", pattern="v")
+#' @export
 
 column_based_recode <- function(df, criterion="all_na", values_from=NULL, 
                                 values_to=NULL, value=0, pattern_type="contains",pattern="Solar",
                                 case_sensitive = FALSE){
-  UseMethod("custom_based_recode")
+  UseMethod("column_based_recode")
   
 }
 
