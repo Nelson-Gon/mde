3a6722f566c85f892fb898f1dbfa083914edc9b0
Nelson-Gon <gonzabato@hotmail.com>
2020-09-12 22:59:42 +0800

    Fix builds

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -2,5 +2,4 @@ testthat::test_that(desc="Test helpers",
                     code = {
 expect_true(get_na_means.data.frame(airquality)[[1]]==percent_missing.data.frame(airquality)[[1]])
 
-                    }
-                      
+                    })
